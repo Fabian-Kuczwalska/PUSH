158bc00bb6c07705ebc5db4dde1a48184cc248a9
Fabian-Kuczwalska <f.kuczwalska@gmail.com>
2025-02-11 13:47:29 +0100

    Setup Project

---
+++ PUSH/PUSHApp.swift
@@ -0,0 +1,17 @@
+//
+//  PUSHApp.swift
+//  PUSH
+//
+//  Created by Fabian Kuczwalska on 11.02.25.
+//
+
+import SwiftUI
+
+@main
+struct PUSHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
